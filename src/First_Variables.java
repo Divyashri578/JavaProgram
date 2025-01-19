9bad69f7c89f01f375f53ef5c6b4080f18eb0c66
Divyashri578 <divyashrimahulkar11@gmail.com>
2025-01-19 20:19:52 +0530

    updated the code

--- src/First_Variables.java
+++ src/First_Variables.java
@@ -6,8 +6,8 @@ public class First_Variables
 
        public static void main(String args[]){
 
-
-        System.out.println("hi");
+       System.out.println("hi");
+           System.out.println("hi");
 
     }
 }
