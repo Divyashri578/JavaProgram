f2ef7234f357ffd39a1d4c8a45fa2f5828e22888
Divyashri578 <divyashrimahulkar11@gmail.com>
2025-02-15 17:53:43 +0530

    updated the code

---
+++ src/Practice_code/Print_Numbers.java
@@ -0,0 +1,13 @@
+package Practice_code;
+
+public class Print_Numbers {
+    public static void main(String[] args) {
+        //int number=0;// no need to define variable, as we alredy define in loop
+        for(int number= 1;number<=10;number++){
+
+
+            System.out.println(number);
+
+        }
+    }
+}
---
+++ src/Practice_code/sumofnumber1to100.java
@@ -0,0 +1,13 @@
+package Practice_code;
+
+public class sumofnumber1to100 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+             sum+=i;
+
+        }
+        System.out.println("sum to 1 to 100 number is:" + sum);
+
+    }
+}
